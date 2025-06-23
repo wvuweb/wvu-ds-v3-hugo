853c11b34e5123965f1ca88089e947749b4187bd
atglenn <adam.glenn@gmail.com>
2025-06-23 17:43:45 -0400

    Updating layouts.

---
+++ assets/js/parvus-gallery--custom.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Setting up the Parvus gallery:
+ * https://github.com/deoostfrees/Parvus
+*/
+
+/* global Parvus */
+
+const prvs = new Parvus({
+  selector: '.js-parvus-lightbox'
+});
